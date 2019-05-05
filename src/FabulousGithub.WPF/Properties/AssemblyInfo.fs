02c5190d26eee1c9ae708631a81032a2f4326d39
Vladislav Khapin <hapinvlad@gmail.com>
2019-05-05 21:00:26 +0300

    init

---
+++ src/FabulousGithub.WPF/Properties/AssemblyInfo.fs
@@ -0,0 +1,24 @@
+﻿namespace FabulousGithub.WPF
+open System.Reflection
+open System.Runtime.CompilerServices
+
+// the name of the type here needs to match the name inside the ResourceDesigner attribute
+//type Resources = FabulousGithub.Droid.Resource
+//[<assembly: Android.Runtime.ResourceDesigner("FabulousGithub.WPF.Resources", IsApplication=true)>]
+
+[<assembly: AssemblyTitle("FabulousGithub.WPF")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
